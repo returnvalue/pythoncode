f9ee55d601656397e7f9eebc99b5f0cdde940f8e
returnvalue <creedaka@hotmail.com>
2020-07-21 16:25:22 -0400

    Update variables.py
    Add Python variables description

--- variables.py
+++ variables.py
@@ -1,3 +1,7 @@
+# Variables are containers for storing data values. Unlike other programming languages
+# Python has no command for declaring a variable. A variable is created the moment you 
+# first assign a value to it.
+
 """
 These keywords are *not* available for use as variable names
 
