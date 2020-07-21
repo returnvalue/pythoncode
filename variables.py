c9b3ccea1a4c05b9dbb0a93e7794d808808ee9d2
gituser <gituser@example.com>
2020-07-21 11:42:29 -0400

    Add reserved keywords to variables.py

---
+++ variables.py
@@ -0,0 +1,10 @@
+"""
+These keywords are *not* available for use as variable names
+
+and	except lambda with as finally nonlocal while assert
+false None yield break for not try class from or is
+continue global	pass else
+def	if elif	raise True
+del	import return in
+
+"""
